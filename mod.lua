2ce8c583e116fc4c12614dd40b8890208dcd4e46
Florian Thiel <flth@flth.de>
2023-09-10 16:54:38 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,28 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "NONE",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "Bandion",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "Car", },
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+	
+	runFn = function (settings)
+	
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,12 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "AI Car Package 2",
+		["mod_desc"] = "Different cars for the AI traffic between 1900 and 1980",
+	},
+	de = {
+		["mod_name"] = "KI Auto Paket 2",
+		["mod_desc"] = "Verschiedene Fahrzeuge für den KI-Verkehr zwischen 1900 und 1980",
+	},
+}
+end
