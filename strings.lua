596bd450cdfe0181b2c56cd1c5a08e982738940e
Florian Thiel <flth@flth.de>
2023-09-11 19:58:00 +0200

    Anpassung Jahreszahlen

--- strings.lua
+++ strings.lua
@@ -2,11 +2,11 @@ function data()
 return {
 	en = {
 		["mod_name"] = "AI Car Package 2",
-		["mod_desc"] = "Different cars for the AI traffic between 1900 and 1980",
+		["mod_desc"] = "Different cars for the AI traffic between 1935 and 1990",
 	},
 	de = {
 		["mod_name"] = "KI Auto Paket 2",
-		["mod_desc"] = "Verschiedene Fahrzeuge für den KI-Verkehr zwischen 1900 und 1980",
+		["mod_desc"] = "Verschiedene Fahrzeuge für den KI-Verkehr zwischen 1935 und 1990",
 	},
 }
 end
